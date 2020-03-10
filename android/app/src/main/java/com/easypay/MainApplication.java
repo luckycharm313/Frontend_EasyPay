4d884e77f8e66b7c3051a6201f016453cf70fa11
luckycharm313 <webmobilepro55@aol.com>
2020-03-10 04:22:43 -0600

    adding react camera

--- android/app/src/main/java/com/easypay/MainApplication.java
+++ android/app/src/main/java/com/easypay/MainApplication.java
@@ -3,6 +3,7 @@ package com.easypay;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.cardio.RNCardIOPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraPackage(),
             new RNCardIOPackage(),
             new VectorIconsPackage(),
             new SvgPackage(),
